fd78e5e51e4bc11dfad0955fee790089429513c7
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-21 22:28:36 +0700

    model siswa

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Siswa;
 use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
     public function index() {
-        return view('siswa.index');
+        $data_siswa = Siswa::all();
+        return view('siswa.index', ['data_siswa' => $data_siswa]);
     }
 }
