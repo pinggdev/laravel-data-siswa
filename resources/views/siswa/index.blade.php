8ab0e95cc90f5a231f8fb54825f964a11577a845
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-21 23:19:31 +0700

    bootstrap

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,21 +1,72 @@
-<h1>Data Siswa</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
 
-<table>
-    <tr>
-        <th>NAMA DEPAN</th>
-        <th>NAMA BELAKANG</th>
-        <th>JENIS KELAMIN</th>
-        <th>AGAMA</th>
-        <th>ALAMAT</th>
-    </tr>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h1>Data Siswa</h1>
+            </div>
 
-    @foreach ($data_siswa as $siswa)
-        <tr>
-            <td>{{ $siswa->nama_depan }}</td>
-            <td>{{ $siswa->nama_belakang }}</td>
-            <td>{{ $siswa->jenis_kelamin }}</td>
-            <td>{{ $siswa->agama }}</td>
-            <td>{{ $siswa->alamat }}</td>
-        </tr>
-    @endforeach
-</table>
+            <div class="col-6">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-sm btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+                    Launch demo modal
+                </button>
+            </div>
+            
+            <table class="table table-hover">
+                <tr>
+                    <th>NAMA DEPAN</th>
+                    <th>NAMA BELAKANG</th>
+                    <th>JENIS KELAMIN</th>
+                    <th>AGAMA</th>
+                    <th>ALAMAT</th>
+                </tr>
+            
+                @foreach ($data_siswa as $siswa)
+                    <tr>
+                        <td>{{ $siswa->nama_depan }}</td>
+                        <td>{{ $siswa->nama_belakang }}</td>
+                        <td>{{ $siswa->jenis_kelamin }}</td>
+                        <td>{{ $siswa->agama }}</td>
+                        <td>{{ $siswa->alamat }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+            ...
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
