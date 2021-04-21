4d5c0a79f056ea4ea9e13b7e8bc052989efb3ba7
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-21 22:40:45 +0700

    view siswa

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,1 +1,21 @@
-<h1>Halo Indonesia</h1>
+<h1>Data Siswa</h1>
+
+<table>
+    <tr>
+        <th>NAMA DEPAN</th>
+        <th>NAMA BELAKANG</th>
+        <th>JENIS KELAMIN</th>
+        <th>AGAMA</th>
+        <th>ALAMAT</th>
+    </tr>
+
+    @foreach ($data_siswa as $siswa)
+        <tr>
+            <td>{{ $siswa->nama_depan }}</td>
+            <td>{{ $siswa->nama_belakang }}</td>
+            <td>{{ $siswa->jenis_kelamin }}</td>
+            <td>{{ $siswa->agama }}</td>
+            <td>{{ $siswa->alamat }}</td>
+        </tr>
+    @endforeach
+</table>
