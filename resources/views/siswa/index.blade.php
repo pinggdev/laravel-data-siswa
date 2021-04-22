d4c3f7e720e26773a88a694f98d96934e81d4c25
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-04-22 22:30:32 +0700

    master blade

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,110 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-</head>
-<body>
-
-    <div class="container">
-        @if (session('sukses'))
-            <div class="alert alert-success" role="alert">
-                {{ session('sukses') }}
-            </div>
-        @endif
-
-        <div class="row">
-            <div class="col-6">
-                <h1>Data Siswa</h1>
-            </div>
-
-            <div class="col-6">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-sm btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
-                    Launch demo modal
-                </button>
-            </div>
-            
-            <table class="table table-hover">
-                <tr>
-                    <th>NAMA DEPAN</th>
-                    <th>NAMA BELAKANG</th>
-                    <th>JENIS KELAMIN</th>
-                    <th>AGAMA</th>
-                    <th>ALAMAT</th>
-                    <th>AKSI</th>
-                </tr>
-            
-                @foreach ($data_siswa as $siswa)
-                    <tr>
-                        <td>{{ $siswa->nama_depan }}</td>
-                        <td>{{ $siswa->nama_belakang }}</td>
-                        <td>{{ $siswa->jenis_kelamin }}</td>
-                        <td>{{ $siswa->agama }}</td>
-                        <td>{{ $siswa->alamat }}</td>
-                        <td><a href="{{ route('siswa.edit', $siswa->id) }}" class="btn btn-info btn-sm">Edit</a></td>
-                    </tr>
-                @endforeach
-            </table>
+@extends('layouts.master')
+@section('content')
+    @if (session('sukses'))
+        <div class="alert alert-success" role="alert">
+            {{ session('sukses') }}
         </div>
+    @endif
+
+    <div class="row">
+        <div class="col-6">
+            <h1>Data Siswa</h1>
+        </div>
+
+        <div class="col-6">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-sm btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+            </button>
+        </div>
+        
+        <table class="table table-hover">
+            <tr>
+                <th>NAMA DEPAN</th>
+                <th>NAMA BELAKANG</th>
+                <th>JENIS KELAMIN</th>
+                <th>AGAMA</th>
+                <th>ALAMAT</th>
+                <th>AKSI</th>
+            </tr>
+        
+            @foreach ($data_siswa as $siswa)
+                <tr>
+                    <td>{{ $siswa->nama_depan }}</td>
+                    <td>{{ $siswa->nama_belakang }}</td>
+                    <td>{{ $siswa->jenis_kelamin }}</td>
+                    <td>{{ $siswa->agama }}</td>
+                    <td>{{ $siswa->alamat }}</td>
+                    <td><a href="{{ route('siswa.edit', $siswa->id) }}" class="btn btn-info btn-sm">Edit</a></td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('siswa.store') }}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Nama Depan</label>
-                            <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Nama Belakang</label>
-                            <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
-                            <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
-                                <option value="L">Laki-Laki</option>
-                                <option value="P">Perempuan</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Agama</label>
-                            <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Alamat</label>
-                            <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                        </div>
-                        
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('siswa.store') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nama Depan</label>
+                        <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nama Belakang</label>
+                        <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+                        <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
+                            <option value="L">Laki-Laki</option>
+                            <option value="P">Perempuan</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Agama</label>
+                        <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Alamat</label>
+                        <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    
                 </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-</body>
-</html>
+    </div>
+@endsection
