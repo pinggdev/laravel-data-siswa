ae34648a9d54d54cbebb8fa24346199cabcd228b
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-05-29 16:40:05 +0700

    halaman muka website

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'SiteController@home');
+Route::get('/about', 'SiteController@about');
 
 
 Route::get('/login', 'AuthController@login')->name('login');
