ac42eea2ee0d14bf70d15255266664c0e687bacd
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-17 16:08:25 +0700

    setting website

---
+++ config/sekolah.php
@@ -0,0 +1,6 @@
+<?php 
+    return [
+        'title' => 'SMA RK BUDI MULIA PEMATANGSIANTAR',
+        'welcome_message' => 'Selamat Datang di Website SMA RK BUDI MULIA PEMATANGSIANTAR'
+    ];
+?>
--- resources/views/sites/home.blade.php
+++ resources/views/sites/home.blade.php
@@ -8,8 +8,7 @@
             <div class="row fullscreen d-flex align-items-center justify-content-between">
                 <div class="banner-content col-lg-9 col-md-12">
                     <h1 class="text-uppercase">
-                        We Ensure better education
-                        for a better world			
+                        {{ config('sekolah.welcome_message') }}
                     </h1>
                     <p class="pt-10 pb-10">
                         In the history of modern astronomy, there is probably no one greater leap forward than the building and launch of the space telescope known as the Hubble.
